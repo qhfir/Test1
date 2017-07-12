add99914522d1b3bd602003cefc1735c0ed1b1a3
liangpengyv <mrliangpengyv@163.com>
2017-07-12 08:37:51 +0800

    添加UI单元测试

---
+++ app/src/main/java/com/example/flow/test1/Calc.java
@@ -0,0 +1,25 @@
+package com.example.flow.test1;
+
+/**
+ * Created by flow on 2017/7/7.
+ */
+
+public class Calc {
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double substract(double a, double b) {
+        return a - b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+}
