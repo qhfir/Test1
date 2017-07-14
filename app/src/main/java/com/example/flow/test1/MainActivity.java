945d691ee9077708c733fd070061d6d2c0cf1727
liangpengyv <mrliangpengyv@163.com>
2017-07-14 14:51:09 +0800

    测试一下

--- app/src/main/java/com/example/flow/test1/MainActivity.java
+++ app/src/main/java/com/example/flow/test1/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findView();   
+        findView();
     }
 
     private void findView() {
